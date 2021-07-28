e3294d69bc03d1e06d3d5e2cd6bb7921ea2d2569
matheusg18 <matheusgabriel.18@gmail.com>
2021-07-28 18:24:43 -0300

    Exercicio de variaveis

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/variaveis.js
@@ -0,0 +1,11 @@
+const myName = "Matheus";
+const birthCity = "Recife";
+const birthYear = 2000;
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
+
+birthYear = 2030;
+
+console.log(birthYear);
