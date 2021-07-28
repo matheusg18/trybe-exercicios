b52ae5d3e1926367e144bdc2f0beda8f535ec45e
matheusg18 <matheusgabriel.18@gmail.com>
2021-07-28 20:06:04 -0300

    Exercicios 8 e 9 feitos

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/programa8.js
@@ -0,0 +1,5 @@
+let a = 93;
+let b = 43;
+let c = 40;
+
+console.log((a * b * c) % 2 == 0);
---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/programa9.js
@@ -0,0 +1,5 @@
+let a = 95;
+let b = 47;
+let c = 43;
+
+console.log(a % 2 == 1 || b % 2 == 1 || c % 2 == 1);
