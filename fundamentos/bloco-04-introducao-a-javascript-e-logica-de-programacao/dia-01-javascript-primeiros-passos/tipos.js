3ced5baa15e3b056b667f911c733edbcaa23ebfb
matheusg18 <matheusgabriel.18@gmail.com>
2021-07-28 18:31:45 -0300

    Exercicio de tipos

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/tipos.js
@@ -0,0 +1,21 @@
+let patientId = 50;
+let isEnrolled = true;
+const patientInfo = {
+  firstName: "Ana",
+  lastName: "Santos",
+};
+const patientEmail = "ana@email.com";
+
+console.log(typeof patientId);
+console.log(typeof isEnrolled);
+console.log(typeof patientInfo);
+console.log(typeof patientEmail);
+console.log(typeof patientAge);
+
+let base = 5;
+let altura = 8;
+let area = base * altura;
+let perimetro = base * 2 + altura * 2;
+
+console.log(area);
+console.log(perimetro);
