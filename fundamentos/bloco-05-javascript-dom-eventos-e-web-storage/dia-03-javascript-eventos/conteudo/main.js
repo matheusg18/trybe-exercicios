9238f6bb884ae3f1520d2489969b0e2ff0e5780f
matheusg18 <matheusgabriel.18@gmail.com>
2021-08-06 17:38:21 -0300

    Requisito 2 do conteudo do bloco 5.3 feito

--- fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-03-javascript-eventos/conteudo/main.js
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-03-javascript-eventos/conteudo/main.js
@@ -1,23 +1,41 @@
-const firstLi = document.getElementById("first-li");
-const secondLi = document.getElementById("second-li");
-const thirdLi = document.getElementById("third-li");
+const firstLi = document.getElementById("firstLi");
+const secondLi = document.getElementById("secondLi");
+const thirdLi = document.getElementById("thirdLi");
 const input = document.getElementById("input");
-const myWebpage = document.getElementById("my-spotrybefy");
+const myWebpage = document.getElementById("mySpotrybefy");
 
 /*
- Copie esse arquivo e edite apenas ele;
- Crie uma função que adicione a classe 'tech' ao elemento selecionado;
-1. Deve existir apenas um elemento com a classe 'tech'. Como você faz isso?
- Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
+1. Copie esse arquivo e edite apenas ele;
+
+2. Crie uma função que adicione a classe 'tech' ao elemento selecionado;
+2.1. Deve existir apenas um elemento com a classe 'tech'. Como você faz isso?
+
+3. Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
 com a classe 'tech';
- Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
+
+4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
 redirecione para alguma página;
-1. Que tal redirecionar para seu portifólio?
- Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
+4.1. Que tal redirecionar para seu portifólio?
+
+5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
 a cor do mesmo;
 
 Segue abaixo um exemplo do uso de event.target:
 */
+
+firstLi.addEventListener("click", addTechClass);
+secondLi.addEventListener("click", addTechClass);
+thirdLi.addEventListener("click", addTechClass);
+
+function addTechClass(event) {
+  let listItems = event.target.parentElement.children;
+
+  for (const item of listItems) {
+    item.removeAttribute("class");
+  }
+
+  event.target.className = "tech";
+}
 
 function resetText(event) {
   // O Event é passado como um parâmetro para a função.
