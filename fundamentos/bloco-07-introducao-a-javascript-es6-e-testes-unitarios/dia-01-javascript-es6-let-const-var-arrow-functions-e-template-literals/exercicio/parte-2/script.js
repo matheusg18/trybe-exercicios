427b4639783d19773f9bfb0531a77f8674f0ba1c
matheusg18 <matheusgabriel.18@gmail.com>
2021-08-27 17:51:49 -0300

    Bloco 7.1 -> Segunda questao da parte 2 do exercicio feito

--- fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-01-javascript-es6-let-const-var-arrow-functions-e-template-literals/exercicio/parte-2/script.js
+++ fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-01-javascript-es6-let-const-var-arrow-functions-e-template-literals/exercicio/parte-2/script.js
@@ -9,3 +9,7 @@
 const factorial = (num) => (num === 0 ? 1 : num * factorial(num - 1));
 
 console.log(factorial(6));
+
+const longestWord = (sentence) => sentence.split(" ").sort((a, b) => b.length - a.length)[0];
+
+console.log(longestWord("Antônio foi no banheiro e não sabemos o que aconteceu"));
