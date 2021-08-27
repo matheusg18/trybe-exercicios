31f17d338a4cf550b5b2de465fed43c7eda14b4b
matheusg18 <matheusgabriel.18@gmail.com>
2021-08-27 16:45:43 -0300

    Bloco 7.1 -> Primeira questao da parte 1 do exercicio feito

---
+++ fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-01-javascript-es6-let-const-var-arrow-functions-e-template-literals/exercicio/parte-1/script1.js
@@ -0,0 +1,13 @@
+const testingScope = (escopo) => {
+  if (escopo) {
+    let ifScope = "Não devo ser utilizada fora do meu escopo (if)";
+    ifScope = `${ifScope} ótimo, fui utilizada no escopo!`;
+    console.log(ifScope);
+  } else {
+    const elseScope = "Não devo ser utilizada fora meu escopo (else)";
+    console.log(elseScope);
+  }
+};
+
+testingScope(true);
+testingScope(false);
