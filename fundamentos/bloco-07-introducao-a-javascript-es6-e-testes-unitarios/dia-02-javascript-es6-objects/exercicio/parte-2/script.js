b5a39d599f9d7487bbc2d04b9cc96e7868c5b268
matheusg18 <matheusgabriel.18@gmail.com>
2021-09-01 09:34:34 -0300

    Bloco 7.2 -> Parte II, exercicio 1 feito

---
+++ fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-02-javascript-es6-objects/exercicio/parte-2/script.js
@@ -0,0 +1,26 @@
+const lesson1 = {
+  materia: 'Matemática',
+  numeroEstudantes: 20,
+  professor: 'Maria Clara',
+  turno: 'manhã',
+};
+
+const lesson2 = {
+  materia: 'História',
+  numeroEstudantes: 20,
+  professor: 'Carlos',
+};
+
+const lesson3 = {
+  materia: 'Matemática',
+  numeroEstudantes: 10,
+  professor: 'Maria Clara',
+  turno: 'noite',
+};
+
+const addEntry = (object, key, value) => {
+  object[key] = value;
+};
+
+addEntry(lesson2, 'turno', 'noite');
+console.table(lesson2);
