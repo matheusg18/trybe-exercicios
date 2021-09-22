ff891c590e458d18e8eacf7eb4784dd875825f99
matheusg18 <matheusgabriel.18@gmail.com>
2021-09-22 17:02:59 -0300

    Bloco 10.1 -> testes criados para myRemove.js

---
+++ fundamentos/bloco-10-testes-automatizados-com-jest/dia-01-primeiros-passos-no-jest/exercicio/parte1/myRemove.test.js
@@ -0,0 +1,22 @@
+const myRemove = require('./myRemove');
+
+describe('Testes de casos normais', () => {
+  it('myRemove([1, 2, 3, 4], 3) deve retornar o array esperado', () => {
+    expect(myRemove([1, 2, 3, 4], 3)).toEqual([1, 2, 4]);
+  });
+  it('myRemove([1, 2, 3, 4], 5) deve retornar o array esperado', () => {
+    expect(myRemove([1, 2, 3, 4], 5)).toEqual([1, 2, 3, 4]);
+  });
+  it('myRemove([1, 2, 3, 4], 3) não deve retornar o array [1, 2, 3, 4]', () => {
+    expect(myRemove([1, 2, 3, 4], 3)).not.toEqual([1, 2, 3, 4]);
+  });
+});
+
+describe('Teste para verificar que o array passado como parâmetro não sofreu alterações', () => {
+  const originalArray = [1, 2, 3, 4, 5];
+  const newArray = myRemove(originalArray, 3);
+
+  it('Testa se o array passado como parâmetro não muda', () => {
+    expect(newArray).not.toEqual(originalArray);
+  });
+});
