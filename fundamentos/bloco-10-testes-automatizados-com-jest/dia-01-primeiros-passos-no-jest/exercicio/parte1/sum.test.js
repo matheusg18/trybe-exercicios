26a513885489086ca35a362e165f003a55b45dbe
matheusg18 <matheusgabriel.18@gmail.com>
2021-09-22 16:46:07 -0300

    Bloco 10.1 -> testes criados para sum.js

---
+++ fundamentos/bloco-10-testes-automatizados-com-jest/dia-01-primeiros-passos-no-jest/exercicio/parte1/sum.test.js
@@ -0,0 +1,16 @@
+const sum = require('./sum');
+
+describe('Testes com valores numéricos', () => {
+  it('sum(4, 5) deve retornar 9', () => {
+    expect(sum(4, 5)).toBe(9);
+  });
+  it('sum(0, 0) deve retornar 0', () => {
+    expect(sum(0, 0)).toBe(0);
+  });
+});
+
+describe('Testa se lança erro com valores não numéricos', () => {
+  it("sum(4, '5') deve lançar o erro 'parameters must be numbers'", () => {
+    expect(() => sum(4, '5')).toThrow('parameters must be numbers');
+  });
+});
