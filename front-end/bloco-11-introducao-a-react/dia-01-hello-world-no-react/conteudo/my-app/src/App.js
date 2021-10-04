3ef13b6f82ce74c61eec494dd657a3e96f4a9d66
matheusg18 <matheusgabriel.18@gmail.com>
2021-10-04 19:06:10 -0300

    Primeiros passos com o React

---
+++ front-end/bloco-11-introducao-a-react/dia-01-hello-world-no-react/conteudo/my-app/src/App.js
@@ -0,0 +1,10 @@
+import Matheus from './Component';
+import './App.css';
+
+function App() {
+  return (
+    <Matheus />
+  );
+}
+
+export default App;
---
+++ front-end/bloco-11-introducao-a-react/dia-01-hello-world-no-react/conteudo/my-app/src/Component.js
@@ -0,0 +1,14 @@
+import { Component } from 'react';
+
+class Matheus extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Matheus</h1>
+        <p>Estudante da Trybe que gosta muito de programar e jogar um lolzinho.</p>
+      </div>
+    );
+  }
+}
+
+export default Matheus;
