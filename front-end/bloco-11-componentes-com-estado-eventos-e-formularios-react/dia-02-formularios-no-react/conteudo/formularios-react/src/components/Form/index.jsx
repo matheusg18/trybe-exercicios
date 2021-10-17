d8328d25f15af3d62414382b2d6207e2ad5bd26f
matheusg18 <matheusgabriel.18@gmail.com>
2021-10-17 11:52:59 -0300

    Conteudo de generic event handlers feito

--- front-end/bloco-11-componentes-com-estado-eventos-e-formularios-react/dia-02-formularios-no-react/conteudo/formularios-react/src/components/Form/index.jsx
+++ front-end/bloco-11-componentes-com-estado-eventos-e-formularios-react/dia-02-formularios-no-react/conteudo/formularios-react/src/components/Form/index.jsx
@@ -5,38 +5,60 @@ export default class Form extends Component {
   constructor() {
     super();
     this.state = {
+      tempo: '',
       senha: '',
+      email: '',
+      textArea: '',
+      check: false,
     };
 
-    this.handlePasswordInput = this.handlePasswordInput.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
 
-  handlePasswordInput(event) {
-    this.setState({ senha: event.target.value });
+  handleChange({ target }) {
+    const { name } = target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+
+    this.setState({ [name]: value });
   }
 
   render() {
-    const { senha } = this.state;
+    const { senha, tempo, email, textArea, check } = this.state;
 
     return (
       <form>
         <label>
           Melhor tempo:
-          <select>
+          <select name="tempo" value={tempo} onChange={this.handleChange}>
             <option value="nublado">Nublado</option>
-            <option value="chuvoso">Chuvoso</option>
+            <option checked value="chuvoso">
+              Chuvoso
+            </option>
             <option value="ensolarado">Ensolarado</option>
           </select>
         </label>
         <label>
-          Email input:
-          <input type="email" />
+          <input type="checkbox" name="check" checked={check} onChange={this.handleChange} />
+          Sim ou não?
         </label>
-        <label>
-          Senha input:
-          <input type="password" value={senha} onChange={this.handlePasswordInput} />
-        </label>
-        <textarea placeholder="Insira texto aqui..." cols="30" rows="10" />
+        <fieldset>
+          <label>
+            Email input:
+            <input name="email" type="email" value={email} onChange={this.handleChange} />
+          </label>
+          <label>
+            Senha input:
+            <input name="senha" type="password" value={senha} onChange={this.handleChange} />
+          </label>
+        </fieldset>
+        <textarea
+          name="textArea"
+          value={textArea}
+          onChange={this.handleChange}
+          cols="30"
+          rows="10"
+        />
+        <input type="file" />
       </form>
     );
   }
