dca639c3766966b28ab1dc02f45abdd766c91a8c
matheusg18 <matheusgabriel.18@gmail.com>
2021-11-15 19:23:49 -0300

    Conteudo -> exercicio 9 feito

--- front-end/bloco-15-gerenciamento-de-estado-com-redux/dia-01-introdução-ao-redux/conteudo/exercicio2/script.js
+++ front-end/bloco-15-gerenciamento-de-estado-com-redux/dia-01-introdução-ao-redux/conteudo/exercicio2/script.js
@@ -1,3 +1,44 @@
+const CHANGE_NAME = 'CHANGE_NAME';
+const CHANGE_LAST_NAME = 'CHANGE_LAST_NAME';
+
+const meuPrimeiroReducer = (state = ESTADO_INICIAL_1, action) => {
+  switch (action.type) {
+    case CHANGE_NAME:
+      const { newName } = action.payload;
+      return {
+        ...state,
+        nome: newName,
+      };
+    case CHANGE_LAST_NAME:
+      const { newLastName } = action.payload;
+      return {
+        ...state,
+        sobrenome: newLastName,
+      };
+    default:
+      return state;
+  }
+};
+
+const meuSegundoReducer = (state = ESTADO_INICIAL_2, action) => {
+  switch (action.type) {
+    case CHANGE_NAME:
+      const { newName } = action.payload;
+      return {
+        ...state,
+        nome: newName,
+      };
+    case CHANGE_LAST_NAME:
+      const { newLastName } = action.payload;
+      return {
+        ...state,
+        sobrenome: newLastName,
+      };
+    default:
+      return state;
+  }
+};
+
 const combinedReducers = Redux.combineReducers({
   reducer1: meuPrimeiroReducer,
   reducer2: meuSegundoReducer,
