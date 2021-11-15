17eb113c0db9b1f066de76f5dcd54316ffece515
matheusg18 <matheusgabriel.18@gmail.com>
2021-11-15 13:26:57 -0300

    Conteudo -> exercicio 3 feito

--- front-end/bloco-15-gerenciamento-de-estado-com-redux/dia-01-introdução-ao-redux/conteudo/script.js
+++ front-end/bloco-15-gerenciamento-de-estado-com-redux/dia-01-introdução-ao-redux/conteudo/script.js
@@ -1,5 +1,20 @@
+const NEXT_COLOR = 'NEXT_COLOR';
+const PREVIOUS_COLOR = 'PREVIOUS_COLOR';
+
 const reducer = (state = ESTADO_INICIAL) => {
+  const len = ESTADO_INICIAL.length;
+
   switch (state.type) {
+    case NEXT_COLOR:
+      return {
+        ...state,
+        index: state.index < len - 1 ? state.index + 1 : state.index,
+      };
+    case PREVIOUS_COLOR:
+      return {
+        ...state,
+        index: state.index > 0 ? state.index - 1 : state.index,
+      };
     default:
       return state;
   }
