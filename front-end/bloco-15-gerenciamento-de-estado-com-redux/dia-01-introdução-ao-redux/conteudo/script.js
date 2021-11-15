01f5fc2c923aadc53f12e5ff6c486e06e65b7f24
matheusg18 <matheusgabriel.18@gmail.com>
2021-11-15 18:57:20 -0300

    Conteudo -> exercicio 6 feito :heavy_check_mark

--- front-end/bloco-15-gerenciamento-de-estado-com-redux/dia-01-introdução-ao-redux/conteudo/script.js
+++ front-end/bloco-15-gerenciamento-de-estado-com-redux/dia-01-introdução-ao-redux/conteudo/script.js
@@ -1,8 +1,9 @@
 const NEXT_COLOR = 'NEXT_COLOR';
 const PREVIOUS_COLOR = 'PREVIOUS_COLOR';
+const RANDOM_COLOR = 'RANDOM_COLOR';
 
 const reducer = (state = ESTADO_INICIAL, action) => {
-  const len = ESTADO_INICIAL.colors.length;
+  const len = state.colors.length;
 
   switch (action.type) {
     case NEXT_COLOR:
@@ -15,6 +16,14 @@ const reducer = (state = ESTADO_INICIAL, action) => {
         ...state,
         index: state.index > 0 ? state.index - 1 : state.index,
       };
+    case RANDOM_COLOR:
+      const newColor = criarCor();
+      const newIndex = len;
+      return {
+        ...state,
+        colors: [...state.colors, newColor],
+        index: newIndex,
+      };
     default:
       return state;
   }
@@ -25,9 +34,20 @@ const store = Redux.createStore(
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
+function criarCor() {
+  const oneChar = ['1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'];
+  let cor = '#';
+  const aleatorio = () => Math.floor(Math.random() * oneChar.length);
+  for (let i = 0; i < 6; i += 1) {
+    cor += oneChar[aleatorio()];
+  }
+  return cor;
+}
+
 window.onload = () => {
   const buttonPrev = document.getElementById('previous');
   const buttonNext = document.getElementById('next');
+  const buttonRandom = document.getElementById('random');
   const colorSpan = document.getElementById('value');
 
   buttonPrev.addEventListener('click', () => {
@@ -36,6 +56,10 @@ window.onload = () => {
 
   buttonNext.addEventListener('click', () => {
     store.dispatch({ type: NEXT_COLOR });
+  });
+
+  buttonRandom.addEventListener('click', () => {
+    store.dispatch({ type: RANDOM_COLOR });
   });
 
   store.subscribe(() => {
