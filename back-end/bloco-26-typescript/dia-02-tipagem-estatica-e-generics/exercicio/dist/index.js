de2ba96f5b946c32072c3d22183ff22a7184cd30
matheusg18 <matheusgabriel.18@gmail.com>
2022-03-21 11:09:35 -0300

    Bloco 26.2 -> Exercicios legado feitos

--- back-end/bloco-26-typescript/dia-02-tipagem-estatica-e-generics/exercicio/dist/index.js
+++ back-end/bloco-26-typescript/dia-02-tipagem-estatica-e-generics/exercicio/dist/index.js
@@ -23,4 +23,60 @@ var Seasons;
     Seasons["SUMMER"] = "Summer";
     Seasons["AUTUMN"] = "Autumn";
 })(Seasons || (Seasons = {}));
-console.log((0, readline_sync_1.question)('oi'));
+const hemispheres = {
+    north: {
+        autumn: {
+            season: Seasons.AUTUMN,
+            months: [Months.SEPTEMBER, Months.OCTOBER, Months.NOVEMBER, Months.DECEMBER],
+        },
+        winter: {
+            season: Seasons.WINTER,
+            months: [Months.DECEMBER, Months.JANUARY, Months.FEBRUARY, Months.MARCH],
+        },
+        spring: {
+            season: Seasons.SPRING,
+            months: [Months.MARCH, Months.APRIL, Months.MAY, Months.JUNE],
+        },
+        summer: {
+            season: Seasons.SUMMER,
+            months: [Months.JUNE, Months.JULY, Months.AUGUST, Months.SEPTEMBER],
+        },
+    },
+    south: {
+        spring: {
+            season: Seasons.SPRING,
+            months: [Months.SEPTEMBER, Months.OCTOBER, Months.NOVEMBER, Months.DECEMBER],
+        },
+        summer: {
+            season: Seasons.SUMMER,
+            months: [Months.DECEMBER, Months.JANUARY, Months.FEBRUARY, Months.MARCH],
+        },
+        autumn: {
+            season: Seasons.AUTUMN,
+            months: [Months.MARCH, Months.APRIL, Months.MAY, Months.JUNE],
+        },
+        winter: {
+            season: Seasons.WINTER,
+            months: [Months.JUNE, Months.JULY, Months.AUGUST, Months.SEPTEMBER],
+        },
+    },
+};
+const getSeasonByMonth = () => {
+    const selectedHemisphere = (0, readline_sync_1.question)('Digite o hemisfério do ano: [north, south]: ');
+    if (!['north', 'south'].includes(selectedHemisphere))
+        return console.log('Hemisfério inválido!');
+    const selectedMonth = (0, readline_sync_1.questionInt)('Digite um mês [1 ~ 12]: ');
+    if (selectedMonth < 1 || selectedMonth > 12)
+        return console.log('Mês inválido!');
+    // que ódio disso
+    const schema = hemispheres[selectedHemisphere];
+    let response = [];
+    for (let s in schema) {
+        const { season, months } = schema[s];
+        if (months.includes(selectedMonth)) {
+            response.push(season);
+        }
+    }
+    return response;
+};
+console.log(getSeasonByMonth());
--- back-end/bloco-26-typescript/dia-02-tipagem-estatica-e-generics/exercicio/index.ts
+++ back-end/bloco-26-typescript/dia-02-tipagem-estatica-e-generics/exercicio/index.ts
@@ -1,4 +1,4 @@
-import { question } from 'readline-sync'
+import { question, questionInt } from 'readline-sync';
 
 enum Months {
   JANUARY = 1,
@@ -22,4 +22,66 @@ enum Seasons {
   AUTUMN = 'Autumn',
 }
 
-console.log(question('oi'));
+const hemispheres = {
+  north: {
+    autumn: {
+      season: Seasons.AUTUMN,
+      months: [Months.SEPTEMBER, Months.OCTOBER, Months.NOVEMBER, Months.DECEMBER],
+    },
+    winter: {
+      season: Seasons.WINTER,
+      months: [Months.DECEMBER, Months.JANUARY, Months.FEBRUARY, Months.MARCH],
+    },
+    spring: {
+      season: Seasons.SPRING,
+      months: [Months.MARCH, Months.APRIL, Months.MAY, Months.JUNE],
+    },
+    summer: {
+      season: Seasons.SUMMER,
+      months: [Months.JUNE, Months.JULY, Months.AUGUST, Months.SEPTEMBER],
+    },
+  },
+
+  south: {
+    spring: {
+      season: Seasons.SPRING,
+      months: [Months.SEPTEMBER, Months.OCTOBER, Months.NOVEMBER, Months.DECEMBER],
+    },
+    summer: {
+      season: Seasons.SUMMER,
+      months: [Months.DECEMBER, Months.JANUARY, Months.FEBRUARY, Months.MARCH],
+    },
+    autumn: {
+      season: Seasons.AUTUMN,
+      months: [Months.MARCH, Months.APRIL, Months.MAY, Months.JUNE],
+    },
+    winter: {
+      season: Seasons.WINTER,
+      months: [Months.JUNE, Months.JULY, Months.AUGUST, Months.SEPTEMBER],
+    },
+  },
+};
+
+const getSeasonByMonth = () => {
+  const selectedHemisphere = question('Digite o hemisfério do ano: [north, south]: ');
+  if (!['north', 'south'].includes(selectedHemisphere)) return console.log('Hemisfério inválido!');
+
+  const selectedMonth = questionInt('Digite um mês [1 ~ 12]: ');
+  if (selectedMonth < 1 || selectedMonth > 12) return console.log('Mês inválido!');
+
+  // que ódio disso
+  const schema = hemispheres[selectedHemisphere as keyof typeof hemispheres];
+
+  let response: string[] = [];
+  for (let s in schema) {
+    const { season, months } = schema[s as keyof typeof schema];
+
+    if (months.includes(selectedMonth)) {
+      response.push(season);
+    }
+  }
+
+  return response;
+};
+
+console.log(getSeasonByMonth());
